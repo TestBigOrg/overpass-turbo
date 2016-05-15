90fd658c0c1e361f4208225a2a352c180e672f6a
Sajjad Anwar <me@sajjad.in>
2016-05-15 16:19:40 +0530

    use mapbox-streets. point to new stack

--- js/configs.js
+++ js/configs.js
@@ -1,16 +1,15 @@
 var configs = {
   appname: "overpass-turbo",
-  defaultServer: "http://ec2-54-89-33-60.compute-1.amazonaws.com/api/",
+  defaultServer: "//overpass-cfn-bots-vpc-302488332.us-east-1.elb.amazonaws.com/api/",
   suggestedServers: [
-    "http://overpass-cfn-staging.tilestream.net/api/",
     "//overpass-api.de/api/",
     "http://overpass.osm.rambler.ru/cgi/",
     "http://api.openstreetmap.fr/oapi/",
   ],
-  defaultTiles: "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  defaultTiles: "//{s}.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbTgzcHQxMzAxMHp0eWx4bWQ1ZHN2NGcifQ.WVwjmljKYqKciEZIC3NfLA",
   tileServerAttribution: "&copy; OpenStreetMap.org contributors&ensp;<small>Data:ODbL, Map:cc-by-sa</small>",
   suggestedTiles: [
-    "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    "//{s}.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbTgzcHQxMzAxMHp0eWx4bWQ1ZHN2NGcifQ.WVwjmljKYqKciEZIC3NfLA",
     //"http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
     //"http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png",
     //"http://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png",
@@ -25,3 +24,6 @@ var configs = {
   short_url_service: "",
   html2canvas_use_proxy: false,
 }
+
+
+// https://a.tiles.mapbox.com/v4/mapbox.streets/3/5/5@2x.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbTgzcHQxMzAxMHp0eWx4bWQ1ZHN2NGcifQ.WVwjmljKYqKciEZIC3NfLA
